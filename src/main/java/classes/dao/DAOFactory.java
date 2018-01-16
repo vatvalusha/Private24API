e2cfdb3ea452d7f9b8a196fef18186c21d8227da
Artemenko Valerii <vatvalusha@gmail.com>
2018-01-16 19:02:48 +0200

    dao service

--- src/main/java/classes/dao/DAOFactory.java
+++ src/main/java/classes/dao/DAOFactory.java
@@ -1,7 +1,27 @@
 package classes.dao;
 
+import classes.interfaceDAO.InformationDAO;
+
 /**
  * Created by Valera on 16.01.2018.
  */
-public class DAOFactory {
+public abstract class DAOFactory {
+    public enum ConnTypes {
+        MongoDBConnection
+//        ConnPoolMySql
+    }
+
+    public static DAOFactory getDaoFactory(ConnTypes connType) {
+        switch (connType) {
+            case MongoDBConnection:
+                return MongoDBConnection.getInstance();
+//            case ConnPoolMySql:
+//                return new ConnPoolMySqlDaoFactory();
+            default:
+                return null;
+        }
+    }
+
+    public abstract InformationDAO getInformationDao();
+
 }
--- src/main/java/classes/dao/MongoDBConnection.java
+++ src/main/java/classes/dao/MongoDBConnection.java
@@ -1,5 +1,7 @@
-package classes;
+package classes.dao;
 
+import classes.MongoDAO.MongoInformation;
+import classes.interfaceDAO.InformationDAO;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
@@ -10,27 +12,33 @@ import java.net.UnknownHostException;
 /**
  * Created by Valera on 15.01.2018.
  */
-public class MongoDBConnection {
+public class MongoDBConnection extends DAOFactory {
+    private static final String MONGO_URL = "mongodb://localhost:27017";
+    static final String DD_NAME = "users";
+
     private static MongoDBConnection ourInstance = new MongoDBConnection();
 
     public static MongoDBConnection getInstance() {
         return ourInstance;
     }
 
-    private MongoDBConnection() {
+    private MongoDBConnection() {}
 
-    }
-
-    public DBCollection getCollection() {
+    public DB getCollection() {
         MongoClient mongoClient = null;
         try {
-            mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-            DB database = mongoClient.getDB("users");
-            DBCollection collection = database.getCollection("information");
-            return collection;
+            mongoClient = new MongoClient(new MongoClientURI(MONGO_URL));
+            DB database = mongoClient.getDB(DD_NAME);
+//            DBCollection collection = database.getCollection("information");
+            return database;
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @Override
+    public InformationDAO getInformationDao() {
+        return new MongoInformation();
     }
 }
