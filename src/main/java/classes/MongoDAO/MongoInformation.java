50cb8c09a4a7ea4435105901a79fbf8f7b8a97e8
Artemenko Valerii <vatvalusha@gmail.com>
2018-01-17 18:27:28 +0200

    realization of nw methodes from interface

--- src/main/java/classes/MongoDAO/MongoInformation.java
+++ src/main/java/classes/MongoDAO/MongoInformation.java
@@ -5,8 +5,15 @@ import classes.entity.User;
 import classes.interfaceDAO.InformationDAO;
 import classes.servlet.logiHandle.UserLogic;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
@@ -21,27 +28,28 @@ public class MongoInformation implements InformationDAO {
 
 
     public MongoInformation() {
-        System.out.println("CONNECTION");
         connection = MongoDBConnection.getInstance();
+        System.out.println("CONNECTED SUCCESSFUL");
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        DB database = connection.getCollection();
-        DBCollection dbCollection = database.getCollection("information");
-        DBCursor cursor = dbCollection.find();
-        List<DBObject> allRecords = new ArrayList<DBObject>();
+        MongoDatabase database = connection.getCollection();
+        MongoCollection<Document> dbCollection = database.getCollection("information");
+        MongoCursor<Document> cursor = dbCollection.find().iterator();
+        List<Document> allRecords = new ArrayList<Document>();
         while (cursor.hasNext()) {
-            DBObject obj = cursor.next();
-            allRecords.add(obj);
+            Document doc = cursor.next();
+            allRecords.add(doc);
         }
-        int count = 1;
-        for (DBObject object : allRecords) {
-            System.out.println("count №" + count + ": " + object);
-        }
-        List<User> users = UserLogic.allUsers(allRecords);
-        return users;
+//        int count = 1;
+//        for (Document object : allRecords) {
+//            System.out.println("count №" + count + ": " + object);
+//        }
+        List<User> list = UserLogic.allUsers(JSON.serialize(allRecords));
+//        list.forEach(x -> System.out.println(x.get_id().get$oid()));
+        return list;
     }
 
     @Override
@@ -50,43 +58,38 @@ public class MongoInformation implements InformationDAO {
     }
 
     @Override
-    public User deleteUser(String id) {
+    public void deleteUser(String id) {
         Gson gson = new Gson();
         User user = new User();
-        DB database = connection.getCollection();
-        DBCollection dbCollection = database.getCollection("information");
-//        DBCursor cursor = dbCollection.find(new BasicDBObject("age", new BasicDBObject("$gt", 10)));
-        BasicDBObject whereQuery = new BasicDBObject();
-        whereQuery.put("name", id);
-        DBCursor cursor = dbCollection.find(whereQuery);
-        System.out.println(cursor);
-        if (cursor.hasNext()) {
-            DBObject object = cursor.next();
-            user = gson.fromJson(object.toString(), User.class);
-//            System.out.println("FOUND!" + cursor.next());
-        }
-//        while (cursor.hasNext()) {
-//            System.out.println(cursor.next());
-//        }
-        return user;
+        MongoDatabase database = connection.getCollection();
+        MongoCollection<Document> dbCollection = database.getCollection("information");
+        dbCollection.deleteOne(new Document("_id", new ObjectId(id)));
     }
 
 
-    //testing
     @Override
     public User insertUser(User newUser) {
-        DB database = connection.getCollection();
-        DBCollection dbCollection = database.getCollection("information");
+        User user = null;
         Gson gson = new Gson();
-        DBObject dbObject = (DBObject) JSON.parse(gson.toJson(newUser));
-        dbCollection.insert(dbObject);
-        DBCursor cursor = dbCollection.find();
-        while (cursor.hasNext()) {
-            DBObject obj = cursor.next();
-            User u = gson.fromJson(obj.toString(), User.class);
-            System.out.println(u.getName());
-        }
-        return null;
+        MongoDatabase database = connection.getCollection();
+        MongoCollection<Document> dbCollection = database.getCollection("information");
+        Document doc = Document.parse(gson.toJson(newUser));
+        dbCollection.insertOne(doc);
+        MongoCursor<Document> cursor = dbCollection.find().iterator();
+        return UserLogic.convertDocToUser(cursor);
+    }
+
+    @Override
+    public void updateUser(User newUser) {
+        MongoDatabase database = connection.getCollection();
+        MongoCollection<Document> dbCollection = database.getCollection("information");
+        String json = UserLogic.convertObjectToJSON(newUser);
+        Document doc = UserLogic.convertJsonToDoc(json);
+        Bson filter = new Document("_id", new ObjectId(newUser.get_id().get$oid()));
+        Bson newValue = doc;
+        Bson updateOperationDocument = new Document("$set", newValue);
+        dbCollection.updateOne(filter, updateOperationDocument);
+//        System.out.println(doc);
     }
 
     /**
@@ -95,7 +98,6 @@ public class MongoInformation implements InformationDAO {
      * @param obj
      * @return
      */
-
     public WriteResult insert(Object obj) {
         Gson gson = new Gson();
         DBObject dbObject = (DBObject) JSON.parse(gson.toJson(obj));
