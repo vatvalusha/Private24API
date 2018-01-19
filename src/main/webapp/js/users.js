2bdc6745452d441dedd9ba1a09f5a15e3a596375
Artemenko Valerii <vatvalusha@gmail.com>
2018-01-19 18:58:51 +0200

    add js functions

--- src/main/webapp/js/users.js
+++ src/main/webapp/js/users.js
@@ -1,3 +1,19 @@
 /**
  * Created by new on 19.01.2018.
  */
+function disableInput(idUser) {
+    console.log("INSIDE");
+    var disable = document.getElementById(idUser).disabled;
+    console.log(disable);
+    if (disable) {
+        console.log("true");
+        document.getElementById('id1').style.display = "none";
+        document.getElementById('id2').style.display = "inline-block";
+        document.getElementById(idUser).disabled = false;
+    } else {
+        console.log("false");
+        document.getElementById('id2').style.display = "none";
+        document.getElementById('id1').style.display = "inline-block";
+        document.getElementById(idUser).disabled = true;
+    }
+}
