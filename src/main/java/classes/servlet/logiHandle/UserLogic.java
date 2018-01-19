349ebbe6d9a197fa39aa682855b76e515ac088ef
Artemenko Valerii <vatvalusha@gmail.com>
2018-01-19 18:57:33 +0200

    logic for Comands

--- src/main/java/classes/servlet/logiHandle/UserLogic.java
+++ src/main/java/classes/servlet/logiHandle/UserLogic.java
@@ -2,7 +2,11 @@ package classes.servlet.logiHandle;
 
 import classes.entity.User;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.mongodb.DBObject;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.util.JSON;
+import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +16,57 @@ import java.util.List;
  */
 public class UserLogic {
 
-    public static List<User> allUsers(List<DBObject> objects){
-        Gson gson=new Gson();
-        List<User> users = new ArrayList<>();
-        for(DBObject object : objects) {
-//                DBObject obj = cursor.next();
-                //Reversal
-                User u = gson.fromJson(object.toString(), User.class);
-                users.add(u);
-                System.out.println(u.getName());
+    /**
+     * Convert input json to List type User
+     *
+     * @param json
+     * @return
+     */
+    public static List<User> allUsers(String json) {
+        Gson gson = new Gson();
+        List<User> list = null;
+        list = gson.fromJson(json, new TypeToken<List<User>>() {
+        }.getType());
+        return list;
+    }
+
+    /**
+     * convert input MongoCursor object to User Object
+     *
+     * @param cursor
+     * @return
+     */
+    public static User convertDocToUser(MongoCursor<Document> cursor) {
+        User user = null;
+        Gson gson = new Gson();
+        while (cursor.hasNext()) {
+            Document document = cursor.next();
+            user = gson.fromJson(JSON.serialize(document), new TypeToken<User>() {
+            }.getType());
         }
-        return users;
+        return user;
+    }
+
+    /**
+     * convert List type Object to Object type String
+     *
+     * @param objects
+     * @return
+     */
+    public static String convertObjectToJSON(Object objects) {
+        Gson gson = new Gson();
+        String json = gson.toJson(objects);
+        return json;
+    }
+
+    /**
+     * convert object type String to Document object
+     *
+     * @param json
+     * @return
+     */
+    public static Document convertJsonToDoc(String json) {
+        Document doc = Document.parse(json.toString());
+        return doc;
     }
 }
